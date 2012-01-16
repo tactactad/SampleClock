f61e864f02330fc0dc4d5928ca110b2d11db7939
Takuro Shimoda <tac@reiare.net>
2012-01-16 16:12:15 +0900

    add board canvas test case.

--- src/js/test.js
+++ src/js/test.js
@@ -1,17 +1,19 @@
 var clock = new SampleClock();
 module('initialize');
 test('initialize', function () {
-    expect(3);
-    deepEqual(clock.canvas.id, 'board');
+    expect(4);
+    deepEqual(clock.board.id, 'board');
+    deepEqual(clock.canvas.id, 'time');
     deepEqual(clock.canvas.width, window.innerWidth);
     deepEqual(clock.canvas.height, window.innerHeight);
 });
 
 module('property');
 test('size', function () {
-    expect(2);
+    expect(3);
     deepEqual(clock.width(), window.innerWidth);
     deepEqual(clock.height(), window.innerHeight);
+    deepEqual(clock.radius(), Math.min(window.innerWidth / 2, window.innerHeight / 2));
 });
 
 module('rad');
