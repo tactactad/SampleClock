52ece21850819d71482edc049378b2badd82981c
Takuro Shimoda <tac@reiare.net>
2012-01-16 13:57:31 +0900

    implement draw board.

--- src/js/sampleClock.js
+++ src/js/sampleClock.js
@@ -1,15 +1,18 @@
-function SampleClock(id) {
+function SampleClock() {
     try {
-        if (!id) id = 'board';
-        this.canvas = document.getElementById(id);
-        this.ctx = this.canvas.getContext('2d');
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
+        this.board = document.getElementById('board');
+        this.boardContext = this.board.getContext('2d');
+        this.canvas = document.getElementById('time');
+        this.context = this.canvas.getContext('2d');
+        this.board.width = this.canvas.width = window.innerWidth;
+        this.board.height = this.canvas.height = window.innerHeight;
 
-        this.ctx.translate(this.width() / 2, this.height() / 2);
+        this.boardContext.translate(this.width() / 2, this.height() / 2);
+        this.context.translate(this.width() / 2, this.height() / 2);
+
+        this.draw_clock();
     } catch (e) {
-        alert('Not found Canvas element.');
-        return;
+        alert('initialize error...');
     }
 }
 SampleClock.prototype = {
@@ -18,6 +21,9 @@ SampleClock.prototype = {
     },
     height: function () {
         return this.canvas.height;
+    },
+    radius: function () {
+        return Math.min(this.width() / 2, this.height() / 2);
     },
     toRad: function (angle) {
         return Math.PI / 180 * angle;
@@ -28,10 +34,47 @@ SampleClock.prototype = {
     },
     secondRad: function (time) {
     },
+
+    draw_time: function () {
+    },
+
     draw_clock: function () {
         console.log('start draw clock...');
+        this.boardContext.beginPath();
+        this.boardContext.scale(0.9, 0.9);
+        this.boardContext.lineWidth = this.radius() * 0.05;
+        this.boardContext.shadowBlur = this.radius() * 0.012;
+        this.boardContext.shadowColor = '#666666';
+        this.boardContext.arc(0, 0, this.radius(), 0, Math.PI * 2, true);
+        this.boardContext.stroke();
+
+        // 時の印
+        this.boardContext.beginPath();
+        this.boardContext.scale(0.9, 0.9);
+        this.boardContext.lineWidth = this.radius() * 0.03;
+        this.boardContext.lineCap = 'round';
+        for (var i = 0; i < 12; i++) {
+            this.boardContext.moveTo(this.radius(), 0);
+            this.boardContext.lineTo(this.radius() * 0.9, 0);
+            this.boardContext.rotate(this.toRad(30));
+        }
+        this.boardContext.stroke();
+
+        // 分の印
+        this.boardContext.beginPath();
+        this.boardContext.lineWidth = this.radius() * 0.02;
+        for (var i = 0; i < 60; i++) {
+            if (i % 5 !== 0) {
+                this.boardContext.moveTo(this.radius(), 0);
+                this.boardContext.lineTo(this.radius() * 0.97, 0);
+            }
+            this.boardContext.rotate(this.toRad(6));
+        }
+        this.boardContext.stroke();
+
         console.log('end draw clock...');
     }
 };
 
 var clock = new SampleClock();
+
